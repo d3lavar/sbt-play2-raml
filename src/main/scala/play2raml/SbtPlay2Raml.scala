f6f633938f95c18d96a1a4498e7c118c9f35d57f
Andrey Biryukov <Andrey.Biryukov@raiffeisen.ru>
2015-11-06 13:35:45 +0300

    task double run fix

--- src/main/scala/play2raml/SbtPlay2Raml.scala
+++ src/main/scala/play2raml/SbtPlay2Raml.scala
@@ -1,14 +1,18 @@
 package play2raml
 
-import com.typesafe.sbt.web.incremental._
+import java.io.File
+
+import org.apache.commons.io.FileUtils
 import play.api.PlayException
 import play.core.PlayVersion
 import play.routes.compiler.RoutesCompiler.RoutesCompilerTask
-import play.routes.compiler.{RoutesCompilationError, RoutesGenerator}
+import play.routes.compiler.{RoutesFileParser, RoutesCompilationError, RoutesGenerator}
 import play.sbt.Play
 import play.sbt.routes._
 import sbt._
 import Keys._
+
+import scala.io.Codec
 
 
 object SbtPlay2Raml extends AutoPlugin {
@@ -31,7 +35,6 @@ object SbtPlay2Raml extends AutoPlugin {
 
   def routesSettings = Seq(
     autoImport.ramlRoutesCompilerTasks <<= Def.taskDyn {
-
       // Aggregate all the routes file tasks that we want to compile the reverse routers for.
       aggregateReverseRoutes.value.map { agg =>
         autoImport.ramlRoutesCompilerTasks in(agg.project, configuration.value)
@@ -54,17 +57,7 @@ object SbtPlay2Raml extends AutoPlugin {
         thisProjectTasks ++ reverseRouterTasks
       }
     },
-  // probably needs to be added
-  /*  watchSources in Defaults.ConfigGlobal <++= sources in routes,
-
-    target in routes := crossTarget.value / "routes" / Defaults.nameForSrc(configuration.value.name),
-
-    routes <<= compileRamlRoutesFiles,
-
-    sourceGenerators <+= routes,
-    managedSourceDirectories <+= target in routes,*/
-
-    sourceGenerators in Compile <+= compileRamlRoutesFiles
+    sourceGenerators <+= compileRamlRoutesFiles
   )
 
   private val compileRamlRoutesFiles = Def.task[Seq[File]] {
@@ -78,16 +71,11 @@ object SbtPlay2Raml extends AutoPlugin {
 
     val (products, errors) = {
       val results = tasks.map { task =>
-        play.routes.compiler.RoutesCompiler.compile(task, generator, generatedDir)
+        compile(task, generator, generatedDir)
       }
-      val t = results.groupBy(_.isRight)
-
-      val files = results.flatMap {
-        case Right(f) => f
-      }
-      val errors = results.flatMap {
-        case Left(e) => e
-      }
+      val (lefts, rights) = results.partition(_.isLeft)
+      val errors = lefts.flatMap { case Left(e) => e }
+      val files = rights.flatMap { case Right(r) => r }
       (files, errors)
     }
 
@@ -102,14 +90,30 @@ object SbtPlay2Raml extends AutoPlugin {
     products.to[Seq]
   }
 
+  def compile(task: RoutesCompilerTask, generator: RoutesGenerator, generatedDir: File): Either[Seq[RoutesCompilationError], Seq[File]] = {
+
+    val namespace = Option(task.file.getName).filter(_.endsWith(".routes")).map(_.dropRight(".routes".size))
+    val routeFile = task.file.getAbsoluteFile
+    val parsed = RoutesFileParser.parse(routeFile)
+    parsed.right.map { rules =>
+      val generated = generator.generate(task, namespace, rules)
+      generated.map {
+        case (filename, content) =>
+          val file = new File(generatedDir, filename)
+          FileUtils.writeStringToFile(file, content, implicitly[Codec].name)
+          println(s"Writing file: $filename")
+          file
+      }
+    }
+  }
 
   private def reportCompilationError(log: Logger, error: PlayException.ExceptionSource) = {
     // log the source file and line number with the error message
     log.error(Option(error.sourceName).getOrElse("") + Option(error.line).map(":" + _).getOrElse("") + ": " + error.getMessage)
-    Option(error.interestingLines(0)).map(_.focus).flatMap(_.headOption) map { line =>
+    Option(error.interestingLines(0)).map(_.focus).flatMap(_.headOption) foreach { line =>
       // log the line
       log.error(line)
-      Option(error.position).map { pos =>
+      Option(error.position).foreach { pos =>
         // print a carat under the offending character
         val spaces = (line: Seq[Char]).take(pos).map {
           case '\t' => '\t'
