13cbb8c510e6957734621eaefc84f31fb6472682
Andrey Biryukov <Andrey.Biryukov@raiffeisen.ru>
2015-11-06 11:13:46 +0300

    gitignore #2

--- target/scala-2.10/sbt-0.13/api/index.js
+++
@@ -1,1 +0,0 @@
-Index.PACKAGES = {"play2raml" : [{"case class" : "play2raml\/RoutesCompilationException.html", "name" : "play2raml.RoutesCompilationException"}, {"object" : "play2raml\/SbtPlay2Raml$.html", "name" : "play2raml.SbtPlay2Raml"}], "sbt" : [], "sbt.serialization" : []};
